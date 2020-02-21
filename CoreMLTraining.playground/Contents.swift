724e3e0e2e62fd28dbf2b47e88d8f15d1967d7e8
Jacopo Mangiavacchi <jamangia@microsoft.com>
2020-02-21 08:56:19 -0800

    WIP update standalone playground

--- CoreMLTraining.playground/Contents.swift
+++ CoreMLTraining.playground/Contents.swift
@@ -1,55 +1,34 @@
 import Foundation
+import CoreML
 
-let coremlModel = Model(version: 4,
-                        shortDescription: "Regression",
-                        author: "Jacopo Mangiavacchi",
-                        license: "MIT",
-                        userDefined: ["SwiftCoremltoolsVersion" : "0.0.3"]) {
-    Input(name: "input", shape: [13])
-    Output(name: "output", shape: [1])
-    TrainingInput(name: "input", shape: [13])
-    TrainingInput(name: "output_true", shape: [1])
-    NeuralNetwork(losses: [MSE(name: "lossLayer",
-                               input: "output",
-                               target: "output_true")],
-                  optimizer: SGD(learningRateDefault: 0.001,
-                                 learningRateMax: 0.3,
-                                 miniBatchSizeDefault: 32,
-                                 miniBatchSizeRange: [32],
-                                 momentumDefault: 0,
-                                 momentumMax: 1.0),
-                  epochDefault: 500,
-                  epochSet: [500],
-                  shuffle: true) {
-        InnerProduct(name: "dense1",
-                     input: ["input"],
-                     output: ["outDense1"],
-                     inputChannels: 13,
-                     outputChannels: 64,
-                     updatable: true)
-        ReLu(name: "Relu1",
-             input: ["outDense1"],
-             output: ["outRelu1"])
-        InnerProduct(name: "dense2",
-                     input: ["outRelu1"],
-                     output: ["outDense2"],
-                     inputChannels: 64,
-                     outputChannels: 32,
-                     updatable: true)
-        ReLu(name: "Relu2",
-             input: ["outDense2"],
-             output: ["outRelu2"])
-        InnerProduct(name: "dense3",
-                     input: ["outRelu2"],
-                     output: ["output"],
-                     inputChannels: 32,
-                     outputChannels: 1,
-                     updatable: true)
-    }
-}
+let folderPath = "/Users/jacopo/CoreMLTraining/"
 
-print(coremlModel)
+let fileURL = Bundle.main.url(forResource: "housing", withExtension: "csv")
+let boston = BostonHousing(fileURL: fileURL!)
 
-let coreMLData = coremlModel.coreMLData
+boston.saveModel(path: folderPath + "model/coreml_no_trained.mlmodel")
 
-print(coreMLData!.count)
+let (untrainedModel, compiledModelUrl) = boston.compileCoreML(path: folderPath + "model/coreml_no_trained.mlmodel")
+
+print("CoreML inference")
+
+print(boston.xTrain[0], boston.yTrain[0], boston.inferenceCoreML(model: untrainedModel, x: boston.xTrain[0]))
+print(boston.xTest[0], boston.yTest[0], boston.inferenceCoreML(model: untrainedModel, x: boston.xTest[0]))
+print(boston.xTest[1], boston.yTest[1], boston.inferenceCoreML(model: untrainedModel, x: boston.xTest[1]))
+print(boston.xTest[17], boston.yTest[17], boston.inferenceCoreML(model: untrainedModel, x: boston.xTest[17]))
+
+
+print("CoreML train")
+boston.train(url: compiledModelUrl, retrainedCoreMLFilePath: folderPath + "model/coreml_trained.mlmodelc")
+
+Thread.sleep(forTimeInterval: 10)
+
+print("Load CoreML Retrained Model")
+let retrainedModel = try! MLModel(contentsOf: URL(fileURLWithPath: folderPath + "model/coreml_trained.mlmodelc"))
+
+print("CoreML inference")
+
+print(boston.xTrain[0], boston.yTrain[0], boston.inferenceCoreML(model: retrainedModel, x: boston.xTrain[0]))
+print(boston.xTest[0], boston.yTest[0], boston.inferenceCoreML(model: retrainedModel, x: boston.xTest[0]))
+print(boston.xTest[1], boston.yTest[1], boston.inferenceCoreML(model: retrainedModel, x: boston.xTest[1]))
+print(boston.xTest[17], boston.yTest[17], boston.inferenceCoreML(model: retrainedModel, x: boston.xTest[17]))
